3fed369796ed186277e7d73444b8778793083a28
Elasticated-Shoe <jmetcalfe24@gmail.com>
2020-07-27 22:14:25 +0100

    Removed Old Build Code, Had To Be Run Manually

--- Jekyll Tools/commandLine.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-def runCommand(command):
-    openCommandHandle = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
-    output, err = openCommandHandle.communicate()
-    return output
